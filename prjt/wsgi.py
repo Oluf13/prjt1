07891e3c247628eb62ccba8340f5d288b5d41955
Oluf13 <86062126+Oluf13@users.noreply.github.com>
2021-06-18 18:14:33 +0200

    Update wsgi.py

--- prjt/wsgi.py
+++ prjt/wsgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vamo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prjt.settings')
 
 application = get_wsgi_application()
