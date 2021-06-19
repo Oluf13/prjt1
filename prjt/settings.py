115102cb67367dde275e764e67c213000f21576b
Oluf13 <86062126+Oluf13@users.noreply.github.com>
2021-06-19 13:49:38 +0200

    Update settings.py

--- prjt/settings.py
+++ prjt/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3gjm^_scxasa&ewe64tb$ass7t)z*a45aycsqnwo3j!+^=xq&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOST = ['vamo13.herokuapp.com']
 
