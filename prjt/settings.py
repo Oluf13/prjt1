a23ee9fd67cffac7a01ac4ebcf99c771fa8882c0
Oluf13 <86062126+Oluf13@users.noreply.github.com>
2021-06-20 17:12:35 +0200

    Update settings.py

--- prjt/settings.py
+++ prjt/settings.py
@@ -88,7 +88,12 @@ WSGI_APPLICATION = 'prjt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # Heroku: Update database configuration from $DATABASE_URL.
 
 db_from_env = dj_database_url.config(conn_max_age=500)
